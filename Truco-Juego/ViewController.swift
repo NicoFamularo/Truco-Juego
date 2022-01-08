11ccdc46861a0d3e6044d220195367f8631352b2
Nicolas Famularo <nicolas.famularo@gmail.com>
2022-01-08 16:56:30 -0300

    Initial Commit

---
+++ Truco-Juego/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Truco-Juego
+//
+//  Created by Nicolas Famularo on 08/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
